5e10d3bc44fbf23fc2c06f8d87d6448ff6953535
ashish <ashishmasih92g@gmail.com>
2025-03-09 15:30:34 +0530

    assignment completed

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    let table = document.querySelector("table")
+let prices = document.getElementsByClassName("price")
+let tr = document.createElement("tr");
+let td1 = document.createElement("td");
+td1.className="total"
+td1.innerHTML="Total Price"
+let td2 = document.createElement("td");
+td2.className="total_price"
+
+let sum =0
+for(let t of prices){
+    sum= sum+parseInt(t.innerHTML);
+    
+}
+td2.innerText=sum
+
+table.append(tr)
+tr.append(td1,td2)
+
+
 };
 
 getSumBtn.addEventListener("click", getSum);
