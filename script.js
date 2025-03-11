589edb5f324b0131d5751feed471675a4c95e6c6
ashish <ashishmasih92g@gmail.com>
2025-03-11 22:47:19 +0530

    assignment completed

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
     let table = document.querySelector("table")
-let prices = document.getElementsByClassName("price")
-let tr = document.createElement("tr");
-let td1 = document.createElement("td");
-td1.className="total"
-td1.innerHTML="Total Price"
-let td2 = document.createElement("td");
-td2.className="total_price"
+    let prices = document.getElementsByClassName("price")
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    td1.className = "total"
+    td1.id = "ans"
+    td1.innerHTML = "Total Price"
+    let td2 = document.createElement("td");
+    td2.className = "total_price"
+    td2.id = "ans"
+    let sum = 0
+    for (let t of prices) {
+        sum = sum + parseInt(t.innerHTML);
 
-let sum =0
-for(let t of prices){
-    sum= sum+parseInt(t.innerHTML);
-    
-}
-td2.innerText=sum
+    }
+    td2.innerText = sum
 
-table.append(tr)
-tr.append(td1,td2)
+    table.append(tr)
+    tr.append(td1, td2)
 
 
 };
