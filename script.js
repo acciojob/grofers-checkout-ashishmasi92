f51198dfb3c43894c5aa38ceeada4a299da27bc2
ashish <ashishmasih92g@gmail.com>
2025-04-23 17:35:39 +0530

    create a assignment

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
+
+
 const getSum = () => {
     let table = document.querySelector("table")
     let prices = document.getElementsByClassName("price")
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     td1.className = "total"
-    td1.id = "ans"
+
+    if (!table || prices.length === 0) return;
+  
     td1.innerHTML = "Total Price"
     let td2 = document.createElement("td");
-    td2.className = "total_price"
-    td2.id = "ans"
+    td2.setAttribute("data-testid", "total-price");
+
     let sum = 0
     for (let t of prices) {
         sum = sum + parseInt(t.innerHTML);
